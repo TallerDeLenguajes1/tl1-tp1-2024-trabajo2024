db44147d74fabff9364792aab3292dca35b8bab6
SofiaaCruz <sofiaacruz187@gmail.com>
2024-03-26 12:55:14 -0300

    Punteros c++

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
-int main(int argc, char const *argv[])
-{
-    std::cout << "hola mundo" <<std::endl;
+int main() {
+    int *pNumero, numero = 4;
+    pNumero = &numero;
+
+    std::cout << "Hola, mundo" << std::endl;
+    std::cout << "El contenido del puntero es: " << *pNumero << std::endl;
+    std::cout << "La direccion del puntero es: " << pNumero << std::endl;
+    std::cout << "La direccion propia del puntero " << &pNumero << std::endl;
+    std::cout << "La direccion de la variable original es: " << &numero << std::endl;
+    std::cout << "El tamanio de memoria almacenado por el puntero es: " << sizeof(*pNumero) << " bytes" << std::endl;
+
     return 0;
 }
