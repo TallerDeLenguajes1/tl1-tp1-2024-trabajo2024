34573b29b8ed825dd6d23641d9dedf973fd6a62d
SofiaaCruz <sofiaacruz187@gmail.com>
2024-03-26 13:44:00 -0300

    Actualizando

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+float cuadrado(float a);
+void cuadradoProcedimiento(float a);
+void invertir(int a, int b);
+void orden(int a, int b);
+
 int main() {
     int *pNumero, numero = 4;
     pNumero = &numero;
@@ -11,5 +16,22 @@ int main() {
     std::cout << "La direccion de la variable original es: " << &numero << std::endl;
     std::cout << "El tamanio de memoria almacenado por el puntero es: " << sizeof(*pNumero) << " bytes" << std::endl;
 
+    float num, numCuadrado;
+    printf("Inserte un numero para obtener su cuadrado\n");
+    scanf("%f", &num);
+    numCuadrado = cuadrado(num);
     return 0;
+}
+
+float cuadrado(float a){
+    return a*a;
+}
+void cuadradoProcedimiento(float a){
+    printf("%p\n%d\n", &a, a);
+}
+void invertir(int a, int b){
+
+}
+void orden(int a, int b){
+
 }
